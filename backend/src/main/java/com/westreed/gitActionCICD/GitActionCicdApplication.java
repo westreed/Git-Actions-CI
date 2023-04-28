e14cf073cecc3cf76154a5fb634bc099427bc35c
JangSeHun <westreed@naver.com>
2023-04-28 15:02:55 +0900

    테스트용 SpringBoot 프로젝트 추가

---
+++ backend/src/main/java/com/westreed/gitActionCICD/GitActionCicdApplication.java
@@ -0,0 +1,13 @@
+package com.westreed.gitActionCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionCicdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/westreed/gitActionCICD/GitActionCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westreed.gitActionCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
